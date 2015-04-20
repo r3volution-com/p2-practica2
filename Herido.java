517c070a0e039d75f7c7e86770d64564e75654d2
themarioga <marioga2512@gmail.com>
2015-04-20 16:56:51 +0200

    asdf

--- Herido.java
+++ Herido.java
@@ -22,9 +22,11 @@ public class Herido extends Paciente{
 				}
 			}
 			if (presente != -1) {
-				if(lesiones[presente].agrava()){
+				lesiones[presente].agrava();
+				return 0;
+				/*if(lesiones[presente].agrava()){
 					return 0;
-				} else return -1;
+				} else return -1;*/
 			} else {
 				for (int i =0; i < lesiones.length; i++){
 					if (lesiones[i] == null){
