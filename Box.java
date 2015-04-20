c8c348e91f8090a434ccd19b71dd342bc0d3a223
themarioga <marioga2512@gmail.com>
2015-04-20 12:54:25 +0200

    Update

--- Box.java
+++ Box.java
@@ -7,4 +7,53 @@ public class Box {
 		if (nHeridos < 0) plazas = null;
 		else plazas = new Herido[nHeridos];
 	}
+	public int disponible() {
+		if (plazas != null){
+			int cont = 0;
+			for (int i = 0; i<plazas.length; i++){
+				if (plazas[i] == null) cont++;
+			}
+			return cont;
+		}else return 0;
+	}
+	public int plazas(){
+		if (plazas != null) return plazas.length;
+		else return 0;
+	}
+	public int ingreso(Herido p) {
+		if (plazas != null && p != null){
+			for (int i = 0; i < plazas.length;i++){
+				if (plazas[i] == null){
+					plazas[i] = p;
+					return i;
+				}
+			}
+			return -1;
+		} else return -1;
+	}
+	public boolean alta(Herido p) {
+		if (plazas != null && p != null){
+			for (int i=0; i < plazas.length;i++){
+				if (plazas[i] != null && plazas[i].equals(p)){
+					plazas[i] = null;
+					return true;
+				}
+			}
+			return false;
+		} else return false;
+	}
+	public int estaIngresado(Herido p) {
+		if (plazas != null && p != null){
+			for (int i = 0; i < plazas.length;i++){
+				if (plazas[i] != null && plazas[i].equals(p)){
+					return i;
+				}
+			}
+			return -1;
+		} else return -1;
+	}
+	public Herido visita(int i){
+		if (plazas != null) return plazas[i];
+		else return null;
+	}
 }
--- Hospital.java
+++ Hospital.java
@@ -1,9 +1,40 @@
 
 public class Hospital extends Clinica {
+	private Box[] boxes;
 	public Hospital(String nombre, int nPlantas, int nHabitaciones, int nBoxes, int nPlazas){	
 		super(nombre, nPlantas);
 		if (nHabitaciones <= 0) nHabitaciones = 2;
 		if (nBoxes <= 0) nBoxes = 2;
 		if (nPlazas <= 0) nPlazas = 2;
+	}
+	public boolean ingreso (Paciente p){
+		
+	}
+	public boolean ingresoUrgente(Herido p){
+		
+	}
+	public boolean alta(Paciente p){
+		
+	}
+	public Datos consulta(Herido p){
+		
+	}
+	public int plazasLibrer() {
+		
+	}
+	public boolean traslado(String n, Hospital d){
+		
+	}
+	public boolean traslado(Herido h){
+		
+	}
+	public double comparaGravedad(){
+		
+	}
+	public double coeficienteCurtosis(){
+		
+	}
+	public double mediaGeometrica(){
+		
 	}
 }
